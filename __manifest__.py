a59c944b881b9b75314237d26d266dfcdc035f94
Abdelrhamaan <abdelrhmanmamdouh76@gmail.com>
2023-06-08 09:33:54 +0300

    hospital managment system

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "HMS Module",
+    "description": "Hospitals Management System",
+    "author": "HMS",
+    "version": "0.1",
+    "category": "uncategorized",
+    "data": ["security/res_groups.xml",
+                "security/ir.model.access.csv",
+                "reports/hms_patient_template.xml",
+                "reports/reports.xml",
+                "views/hms_patient_view.xml",
+                "views/hms_doctor_view.xml", "views/hms_depa_view.xml",
+                "views/hms_customer_inherit_view.xml"],
+    "depends": ["base"]
+}
+
+# "views/hms_customer_inherit_view.xml"
